59190e54f6ede73935bfa1a5c5af43acd53d224c
Daniel Azimi <danielazimi101@gmail.com>
2022-07-08 15:50:41 -0400

    Added animation and quit button to GUI.

--- nidaqmx_gui.py
+++ nidaqmx_gui.py
@@ -1,29 +1,45 @@
 import tkinter as tk
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.animation as animation
+import nidaqmx_machine
+import time
 
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
+        self.master = master
+        self.init_window()
 
-        self.assembleWidgets()
+    def init_window(self):
 
-    def assembleWidgets(self):
-
-        self.figure = Figure()
-        self.canvas = FigureCanvasTkAgg(self.figure, self)
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self, pack_toolbar=False)
-
+        self.master.title("nidaqmx gui test")
+        self.fig = Figure()
+        # Figure pre configuration
+        self.sampling_time = .25
+        self.n = 100
+        self.x_len = self.n
+        self.v_min = 0
+        self.v_max = .1
+        self.y_range = [self.v_min, self.v_max]
+        # Configure figure
+        self.ax = self.fig.add_subplot(111)
+        self.xs = list(range(0, self.n))
+        self.ys = [0]*self.x_len
+        self.ax.set_ylim(self.y_range)
+        self.line, = self.ax.plot(self.xs, self.ys)
+        # Grid figure
+        self.canvas = FigureCanvasTkAgg(self.fig, self)
+        self.canvas.get_tk_widget().grid(row=0, column=0)
+        # Grid buttons
         self.btn_start = tk.Button(self, text="Start")
         self.btn_stop = tk.Button(self, text="Stop")
-
-        self.canvas.get_tk_widget().grid(row=0,column=0)
-        self.toolbar.grid(row=1,column=0)
-        self.btn_start.grid(row=1,column=1)
-        self.btn_stop.grid(row=1,column=2)
-
-        self.axes = self.figure.add_subplot(111)
-        self.axes.set_ylim(0,1)
+        self.btn_quit = tk.Button(self, text="Quit", command=self.quit)
+        self.btn_start.grid(row=1, column=1)
+        self.btn_stop.grid(row=1, column=2)
+        self.btn_quit.grid(row=2, column=1)
+        self.ani = animation.FuncAnimation(self.fig, nidaqmx_machine.logging, fargs=(
+        self.ys, self.sampling_time, self.line, self.x_len,))
 
 if __name__ == '__main__':
     root = tk.Tk()
--- nidaqmx_machine.py
+++ nidaqmx_machine.py
@@ -12,28 +12,10 @@ def read_daq():
     task.close()
     return value
 
-sampling_time = 1
-n = 10
-x_len = n
-t_min = 0; t_max =.1
-y_range = [t_min, t_max]
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-xs = list(range(0, n))
-ys = [0]*x_len
-ax.set_ylim(y_range)
-line, = ax.plot(xs, ys)
-plt.grid()
-
-def logging(i, ys):
+def logging(i, ys, sampling_time, line, x_len):
     value = read_daq()
     time.sleep(sampling_time)
     ys.append(value)
     ys = ys[-x_len:]
     line.set_ydata(ys)
     return line
-
-ani = animation.FuncAnimation(fig, logging, fargs=(ys,), interval=100)
-
-plt.show()
